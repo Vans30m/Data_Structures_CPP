266101f22ada8f384caa4d799a36f0b48f4b6e53
Vansh Thakur <vthakur.290905@gmail.com>
2025-07-14 22:06:19 +0530

    First Bit
    First Bit

---
+++ 02-07-25/FirstBit.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int getFirstBit(int n){
+	if(n==0) return -1;
+	int ans = 1;
+	while(n % 2 == 0){   // checks least significant bit is zero
+		n = n >> 1;		// if YES, increments ans
+		ans++ ;			// continues, until least significant bit is 1
+	}
+	return ans;
+}
+
+int main(){
+	int n;
+	cout << "Take input number : " << endl;
+	cin >> n;
+	cout << getFirstBit(n) << endl ;
+	
+	return 0;
+}
