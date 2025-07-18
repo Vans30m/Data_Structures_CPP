382459118560ce854ac0128d7e6b04fea32b3f67
Vansh Thakur <vthakur.290905@gmail.com>
2025-07-18 20:13:54 +0530

    18-07-25 new
    18-07-25 new

---
+++ 18-07-25/Login_System.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool login(string name, string password){
+	cout << "Input username : ";
+	string naam;
+	cin >> naam;
+
+	for(int i=0;i<3;i++){			
+		cout << "Input password : ";
+		string in;
+		cin >> in;
+		
+		if(password == in && name == naam){
+			cout << "True";
+			break;
+		}
+		else {
+			cout << "False" << endl;
+		}
+	}
+	return 0;
+}
+
+int main(){
+	
+	login("RAM","ffff");
+		
+}
